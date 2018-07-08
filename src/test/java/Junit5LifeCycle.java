8089ac2ee5978593175b2342bda2d30bcb234b92
harshit <harshittrivedi27@gmail.com>
2018-07-08 13:58:19 +0530

    this is my initial commit

---
+++ src/test/java/Junit5LifeCycle.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class Junit5LifeCycle {
+
+	@BeforeAll
+	static void setup() {
+		System.out.println("@BeforeAll executed");
+	}
+
+	@BeforeEach
+	void setupThis() {
+		System.out.println("@BeforeEach executed");
+	}
+
+	@Test
+	void testCalcOne() {
+		System.out.println("======TEST ONE EXECUTED=======");
+
+	}
+
+	@Test
+	@Disabled
+	void testCalcTwo() {
+		System.out.println("======TEST TWO EXECUTED=======");
+
+	}
+
+	@AfterEach
+	void tearThis() {
+		System.out.println("@AfterEach executed");
+	}
+
+	@AfterAll
+	static void tear() {
+		System.out.println("@AfterAll executed");
+	}
+	
+}
